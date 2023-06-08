1cf3cf018770afbb803b0f4d7c5b840ef2bca3c0
szymonczopek <szymon.czopek@wp.pl>
2023-06-08 13:43:56 +0200

    inital commit

---
+++ controllers/NBPAPI.php
@@ -0,0 +1,22 @@
+<?php
+
+class NBPAPI {
+    private $urlRatesAll = 'http://api.nbp.pl/api/exchangerates/tables/A?format=json';
+
+    public function getAll() {
+        $url = $this->urlRatesAll;
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+        return $data;
+    }
+}
+
+$nbpApi = new NBPAPI();
+
+$ratesAll = $nbpApi->getAll();
+$ratesAll[] = array('message'=> 'Zaimportowano pomyślnieee.');
+header('Content-Type: application/json');
+
+echo json_encode($ratesAll);
+
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>API NBP Szymon Czopek</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="mainDiv">
+        <div>
+            <h1>API NBP</h1>
+        </div>
+        <div>
+            <button type="button" id="buttonGetAll">Pobierz kursy walut</button>
+        </div>
+    </div>
+<script>
+    const buttonGetAll = document.getElementById('buttonGetAll');
+    const messageDiv = document.createElement('div');
+    const mainDiv = document.getElementById('mainDiv');
+
+    buttonGetAll.addEventListener('click', async () => {
+         var isError = false;
+         var message = '';
+        await fetch('http://localhost:63342/api_nbp/controllers/nbpapi.php', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'GET',
+        })
+            .then(async (response) => await response.json())
+            .then(async (data) => {
+                if (data.error) {
+                    const error = `<p>${data.error}</p>`
+                    messageDiv.innerHTML = error;
+                }
+
+                const rates = data[0]['rates'];
+                message = data[1]['message']
+                rates.forEach(element => console.log(element['currency'] + ' ' + element['mid']))
+            })
+            .catch((error) => {
+                isError = true;
+            });
+
+        messageDiv.textContent = message;
+        if (isError) {
+            messageDiv.style.color = 'red';
+        }
+        else messageDiv.style.color = 'black';
+       mainDiv.after(messageDiv);
+
+    })
+
+</script>
+</body>
+</html>
