3a2a46dbf563af67c66757b7509b1c8bc4ebf16d
szymonczopek <szymon.czopek@wp.pl>
2023-06-08 18:27:26 +0200

    display table

--- index.php
+++ index.php
@@ -15,10 +15,50 @@
             <button type="button" id="buttonGetAll">Pobierz kursy walut</button>
         </div>
     </div>
+
+    <table id="TableDiv">
+        <thead id="TableHead">
+
+        </thead>
+        <tbody id="TableBody">
+        </tbody>
+    </table>
 <script>
     const buttonGetAll = document.getElementById('buttonGetAll');
     const messageDiv = document.createElement('div');
     const mainDiv = document.getElementById('mainDiv');
+    const tableDiv = document.getElementById('TableDiv');
+    const tableBody = document.getElementById('TableBody');
+
+    function displayHeader(headers){
+        var TableHeadDiv = document.getElementById("TableHead");
+        const tableHeaderRow = document.createElement('tr');
+
+        headers.forEach((element)=>{
+            const label = document.createElement('th');
+            label.textContent = element;
+            tableHeaderRow.appendChild(label);
+        })
+        TableHeadDiv.innerHTML= '';
+        TableHeadDiv.appendChild(tableHeaderRow);
+    }
+
+    function displayRates(data){
+        const headers = ['Currency','Code','Value'];
+        displayHeader(headers);
+        var counter = 1
+
+        data.forEach((row) => {
+            const newRow = document.createElement('tr');
+            Object.keys(row).forEach(cell => {
+                const newCell = document.createElement('td');
+                newCell.textContent = row[cell];
+                newRow.appendChild(newCell);
+                counter++;
+            })
+            tableBody.append(newRow);
+        })
+    }
 
     buttonGetAll.addEventListener('click', async () => {
          var isError = false;
@@ -37,8 +77,9 @@
                 }
 
                 const rates = data[0]['rates'];
-                message = data[1]['message']
-                rates.forEach(element => console.log(element['currency'] + ' ' + element['mid']))
+                message = data[1]['message'];
+                //rates.forEach(element => console.log(element['currency'] + ' ' + element['mid']));
+                displayRates(rates);
             })
             .catch((error) => {
                 isError = true;
@@ -49,8 +90,7 @@
             messageDiv.style.color = 'red';
         }
         else messageDiv.style.color = 'black';
-       mainDiv.after(messageDiv);
-
+        TableDiv.after(messageDiv)
     })
 
 </script>
